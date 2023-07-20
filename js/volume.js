584d6b1dc4f6d151e0e26bad88846c3d1944a264
LAPTOP-92BHJ75R\hoanghuy <phunghoanghuy8@gmail.com>
2023-07-20 16:39:54 +0700

    volume

--- js/volume.js
+++ js/volume.js
@@ -10,8 +10,10 @@ const audioList = [
     'aseets/img/AlwaysWithMeSpiritedAwayOST-JoeH_3he4c.mp3',
     'aseets/img/CityWithAnOceanViewKiki39sDeli_37vkb.mp3',
     'aseets/img/TonariNoTotoroMyNeighborTotoro-A_37bm3.mp3',
+    'aseets/img/MerryGoRound-JoeHisaishi-2706190.mp3',
+    'aseets/img/CityWithAnOceanViewKiki39sDeli_37vkb.mp3',
+    'aseets/img/TonariNoTotoroMyNeighborTotoro-A_37bm3.mp3',
     'aseets/img/MerryGoRound-JoeHisaishi-2706190.mp3'
-    
   ];
 
 let audio = new Audio(audioList[currentTrack]);
